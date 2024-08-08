b383c3c6a9cf915ac72f1425dbb2417562969d45
Dambasan <yusuf.dambasan@std.yildiz.edu.tr>
2024-08-08 18:47:08 +0300

    Kullanıcı hesap ekleme, register, login, işlemleri için kod ve arayüz tasarımı,

---
+++ src/main/java/com/example/demo/service/MyUserDetailsService.java
@@ -0,0 +1,26 @@
+package com.example.demo.service;
+
+import com.example.demo.MyUserPrincipal;
+import com.example.demo.models.User;
+import com.example.demo.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class MyUserDetailsService implements UserDetailsService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String email) {
+        User user = userRepository.findByEmail(email);
+        if (user == null) {
+            throw new UsernameNotFoundException(email);
+        }
+        return new MyUserPrincipal(user);
+    }
+}
